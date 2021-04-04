ff550423c678b52e07c7f62ee48200c1ecbee058
kfire <kfirettinger@gmail.com>
2021-04-04 20:57:13 +0300

    change print output and add third child

--- task1/part_B/main2_2.c
+++ task1/part_B/main2_2.c
@@ -6,25 +6,34 @@
 char child_stack[10000+1];
 
 void print(const char * text){
-
     for(int i=0 ; i<1000 ;i++){
-        printf("hello from %s with pid - %ld \n",text, getpid());
+        printf("hello from %s\n",text);
         usleep(1000000);
     }
 }
 int child1(void * params){
-    print("child_thread 1 ");
+    usleep(1000000);
+    print("child1");
 }
 int child2(void * params){
-    print("child_thread 2 ");
+    usleep(1000000);
+    print("child2 ");
+}
+int child3(void * params){
+    usleep(1000000);
+    print("child3 ");
 }
 
 int main(){
 
     int result1 = clone(child1,child_stack+10000,CLONE_PARENT,0);
     int result2 = clone(child2,child_stack+10000,CLONE_PARENT,0);
-    printf("clone result1 = %d \n",result1);
-    printf("clone result2 = %d \n",result2);
+    int result3 = clone(child3,child_stack+10000,CLONE_PARENT,0);
+
+    printf("pid of child1 = %d \npid of child2 = %d \npid of child3 = %d \npid of parent = %ld \n--------------------\n",\
+            result1, result2, result3, getpid());
+
+    usleep(1000000);
     print("parent");
 
     return 0;
