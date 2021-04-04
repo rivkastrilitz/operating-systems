ce7e5e704db0bbfa65b7d93d981f65416d6f2875
rivkastrilitz <rivk1471@gmail.com>
2021-04-04 21:11:46 +0300

    updated makefile and almost finished main2_3

---
+++ task1/part_B/main2_3.c
@@ -0,0 +1,40 @@
+#include <sys/types.h> 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+int main(){
+
+    // create child that will be the deamon 
+    pid_t pid = fork();
+
+    if(pid==0){
+
+        // move to root dir 
+        chdir("/");
+
+        // move the child to another session 
+        setsid();
+
+        printf("starting deamon\n");
+
+        // close the output chanels
+        close(stdout);
+        close(stdin);
+        close(stderr);
+
+        // open log
+        openlog("my_deamon",LOG_PID,LOG_DAEMON);
+        // generate a log message
+        syslog(LOG_NOTICE,"deamon started");
+        usleep(3000000);
+        syslog(LOG_NOTICE,"working...");
+        usleep(3000000);
+        syslog(LOG_NOTICE,"deamon finished");
+    }else{
+        printf("deamon pid %d\n",pid);
+    }
+
+    return 0;
+}
