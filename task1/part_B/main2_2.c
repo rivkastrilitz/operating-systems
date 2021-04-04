6a7a530f6db52f146ce7a67afe76af9969c82578
kfire <kfirettinger@gmail.com>
2021-04-04 20:11:27 +0300

    finish 3 proccess with shared memory task

--- task1/part_B/main2_2.c
+++ task1/part_B/main2_2.c
@@ -8,20 +8,25 @@ char child_stack[10000+1];
 void print(const char * text){
 
     for(int i=0 ; i<1000 ;i++){
-        printf("hello from %s pid is %ld \n",text,getpid());
+        printf("hello from %s with pid - %ld \n",text, getpid());
         usleep(1000000);
-
     }
 }
-int child(void * params){
-    print("child_thread");
+int child1(void * params){
+    print("child_thread 1 ");
+}
+int child2(void * params){
+    print("child_thread 2 ");
 }
 
 int main(){
 
-    int result = clone(child,child_stack+10000,CLONE_PARENT,0);
-    printf("clone result = %d  pid is %ld \n",result,getpid());
+    int result1 = clone(child1,child_stack+10000,CLONE_PARENT,0);
+    int result2 = clone(child2,child_stack+10000,CLONE_PARENT,0);
+    printf("clone result1 = %d \n",result1);
+    printf("clone result2 = %d \n",result2);
     print("parent");
+
     return 0;
 
 }
