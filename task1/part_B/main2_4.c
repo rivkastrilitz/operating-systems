b96cb94b59e2b807723963c76e89d867d56e5a3c
rivkastrilitz <rivk1471@gmail.com>
2021-04-05 14:22:47 +0300

    fixde main4 and added usleep

--- task1/part_B/main2_4.c
+++ task1/part_B/main2_4.c
@@ -7,25 +7,26 @@
 char child_stack[10000+1];
 
 int child1(void * params){
-    char* args[2]={"./fork",NULL};
+    char* args[2]={"./main2_1",NULL};
     execvp(args[0],args);
     
 }
 int child2(void * params){
-    char* args[2]={"./clone",NULL};
+    char* args[2]={"./main2_2",NULL};
     execvp(args[0],args);
    
 }
 int child3(void * params){
-   char* args[2]={"./deamon",NULL};
+   char* args[2]={"./main2_3",NULL};
     execvp(args[0],args);
 }
 
 int main (){
-    printf("main pid %d",getpid());
-    int result1 = clone(child1,child_stack+10000,CLONE_PARENT,0);
-    int result2 = clone(child2,child_stack+10000,CLONE_PARENT,0);
-    int result3 = clone(child3,child_stack+10000,CLONE_PARENT,0);
-    usleep(300000);
+    printf("main p %d\n",getpid());
+    printf("main ppid---------------- %d",getppid());
+    int result1 = clone(child1,child_stack+10000,0,0);
+    int result2 = clone(child2,child_stack+10000,0,0);
+    int result3 = clone(child3,child_stack+10000,0,0);
+    usleep(30000000);
     return 0;
 }
